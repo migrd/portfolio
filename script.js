8204b12b4b886bf0bb64830cda8f9e3d11089f78
migrd <miguel.hwe@gmail.com>
2021-09-15 13:38:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,18 +14,17 @@ const navSlide = () => {
 	const navbar = document.querySelector(".nav-bar");
 	const navLinks = document.querySelectorAll(".nav-bar li");
 
-  hamburger.onclick = () => {
-		
+  hamburger.onclick = () => {	
 	  navbar.classList.toggle("nav-active");
 		 
-    //Animation links
+      //Animation links
 	  navLinks.forEach((link, index) => {
 	    if (link.style.animation) {
-			  link.style.animation = "";
-		  } else {
+		  link.style.animation = "";
+		} else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
-		  }
-		});
+		}
+	  });
 	  //hamburger animation
 	  hamburger.classList.toggle("toggle");
   }
